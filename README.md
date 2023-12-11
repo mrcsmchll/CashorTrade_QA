40eeea43034a9ad710b7c857dfaa65942058ae85
mrcsmchll <manchinelli.m@gmail.com>
2023-12-11 16:30:25 -0300

    functioning acces to performers event. Selling of tickets WIP

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
     pip install -r requirements.txt
     ```
 ## Currently tested in a Chromium enviorment
+### To execute test run 'pytest' command in root
 
--- pages/account.py
+++ pages/account.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
+from utils.user import User
 
 class Account:
     URL = "https://front-stage.cashortrade.org/auth/account"
@@ -16,6 +17,19 @@ class Account:
     BTN_SKIP_PIC = "btn-secondary-profile-pic-skip-for-now"
     BTN_SELL = "btn-sell-tickets-header"
     BTN_SKIP_WLK = (By.ID, "btn-secondary-walkthrough-skip")
+    BTN_EVENT = (By.ID, "search-event-result-0")
+    BTN_EVENT_SELL = (By.ID, "sellortrade-sell")
+    BTN_DIGITAL_TRANSF = (By.CSS_SELECTOR, ".relative:nth-child(1) > .border-div")
+    BTN_NEXT = (By.ID, "btn-create-listing-transferType")
+    DROPDOWN_QUANT = (By.ID, "input-dropdown-quantity-select")
+    DROPDOWN_OG_PURCHASE = (By.CSS_SELECTOR, "#input-dropdown-ticket-wallet-primary-vendor > .absolute-full")
+    DROPDOWN_LOC = (By.ID, "input-dropdown-ticket-wallet-ga-section")
+    INPUT_PRICE = (By.ID, "input-number-create-listing-price-per")
+    BTN_ADD_PAY = (By.CLASS_NAME, "block w-full sm:w-auto rounded-lg border-2 border-primary border-solid px-6 py-3 relative flex justify-center items-center svelte-1cvyoxz")
+    INPUT_CARD_NUM = (By.ID, "Field-numberInput")
+    BTN_NEXT_ADD_PAY = (By.ID, "btn-create-listing-creditCardInformation")
+    BTN_NEXT_CREATE_LIST = (By.ID,"btn-create-listing-ticketListing")
+    BTN_POST = (By.ID, "btn-create-listing-listingReview")
     WALLET_WRAPPER = (By.ID, "ticket-wallet-wrapper")
 
 
@@ -84,40 +98,49 @@ class Account:
         if perfs_with_event:        
         #select the first performer found that has registered an event
             perfs_with_event[0].click()
-            self.driver.implicitly_wait(2)
+            #  WebDriverWait(self.driver, 10).until(
+            #     EC.visibility_of_element_located(self.BTN_EVENT)
+            # )    
 
-            event = self.driver.find_element(By.ID, "search-event-result-0")
-            event.click()
-            self.driver.implicitly_wait(2)
+            # event = self.driver.find_element(*self.BTN_EVENT)
+            # event.click()
+            WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(self.BTN_EVENT_SELL)
+            ) 
 
-            sell_btn = self.driver.find_element(By.ID, "sellortrade-sell")
+            sell_btn = self.driver.find_element(*self.BTN_EVENT_SELL)
             sell_btn.click()
-            self.driver.implicitly_wait(2)
+            # WebDriverWait(self.driver, 10).until(
+            #     EC.visibility_of_element_located(self.BTN_DIGITAL_TRANSF)
+            # )
 
-            digital_transfer_btn = self.driver.find_element(By.ID, "relative rounded-lg px-4.5 py-3 group  svelte-1i2ok0t")
+            digital_transfer_btn = self.driver.find_element(*self.
+            BTN_DIGITAL_TRANSF)
             digital_transfer_btn.click()
-            self.driver.implicitly_wait(2)
+           
+            
+            next_btn = self.driver.find_element(*self.BTN_NEXT)
+            next_btn.click()
+            WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(self.DROPDOWN_QUANT)
+            )
 
-
-            submit_btn = self.driver.find_element(By.ID, "btn-create-listing-transferType")
-            submit_btn.click()
-            self.driver.implicitly_wait(2)
-
-            quantity_dropdown = self.driver.find_element(By.ID, "input-dropdown-quantity-select")
+            quantity_dropdown = self.driver.find_element(*self.DROPDOWN_QUANT)
             quantity_dropdown.click()
             self.driver.implicitly_wait(2)
-
             quantity_amount_1 = self.driver.find_element(By.ID, "input-dropdown-option-0")
             quantity_amount_1.click()
             self.driver.implicitly_wait(2)
 
             ready_btn = self.driver.find_element(By.ID, "btn-selector-ready")
             ready_btn.click()
-            self.driver.implicitly_wait(2)
-
-            og_purchase = self.driver.find_element(By.ID, "input-dropdown-ticket-wallet-primary-vendor")
+            WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(self.DROPDOWN_OG_PURCHASE)
+            )
+           
+            og_purchase = self.driver.find_element(*self.DROPDOWN_OG_PURCHASE)
+            self.driver.execute_script("arguments[0].scrollIntoView();", og_purchase)
             og_purchase.click()
-            self.driver.implicitly_wait(2)
 
             og_purchase_1 = self.driver.find_element(By.ID, "input-dropdown-option-0")
             og_purchase_1.click()
@@ -126,8 +149,11 @@ class Account:
             ticket_info = self.driver.find_element(By.ID, "btn-selector-general-admission")
             ticket_info.click()
             self.driver.implicitly_wait(2)
+            WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(self.DROPDOWN_LOC)
+            )
 
-            ticket_loc_dropdown = self.driver.find_element(By.ID, "input-dropdown-ticket-wallet-ga-section")
+            ticket_loc_dropdown = self.driver.find_element(*self.DROPDOWN_LOC)
             ticket_loc_dropdown.click()
             self.driver.implicitly_wait(2)
 
@@ -135,25 +161,33 @@ class Account:
             ticket_loc_selection.click()
             self.driver.implicitly_wait(2)
 
-            submit_btn = self.driver.find_element(By.ID, "btn-create-listing-mediumSpecificInfo")
-            submit_btn.click()
-            self.driver.implicitly_wait(5)
+            next_btn = self.driver.find_element(By.ID, "btn-create-listing-mediumSpecificInfo")
+            next_btn.click()
+            WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(self.INPUT_PRICE)
+            )
 
-            price_input = self.driver.find_element(By.ID, "input-number-create-listing-price-per")
+            price_input = self.driver.find_element(*self.INPUT_PRICE)
             price_input.send_keys("12")
 
-            face_value_radio = self.driver.find_element(By.NAME, "Face Value")
+            face_value_radio = self.driver.find_element(By.CSS_SELECTOR, ".space-y-3 > .flex:nth-child(1)")
+            self.driver.execute_script("arguments[0].scrollIntoView();", face_value_radio)
             face_value_radio.click()
 
-            submit_btn = self.driver.find_element(By.ID, "btn-create-listing-ticketPricing")
-            submit_btn.click()
-            self.driver.implicitly_wait(5)
-
-            add_payment = self.driver.find_element(By.ID, "block w-full sm:w-auto rounded-lg border-2 border-primary border-solid px-6 py-3 relative flex justify-center items-center svelte-1cvyoxz")
+            next_btn = self.driver.find_element(By.ID, "btn-create-listing-ticketPricing")
+            next_btn.click()
+            WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(self.BTN_ADD_PAY)
+            )            
+                     
+            #TODO: Change locator for payment element
+            add_payment = self.driver.find_element(*self.BTN_ADD_PAY)
             add_payment.click()
-            self.driver.implicitly_wait(5)
+            WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(self.INPUT_CARD_NUM)
+            ) 
 
-            card_number_input = self.driver.find_element(By.ID, "Field-numberInput")
+            card_number_input = self.driver.find_element(*self.INPUT_CARD_NUM)
             card_number_input.send_keys("4242424242424242")
 
             expiration_number_input = self.driver.find_element(By.ID, "Field-expiryInput")
@@ -165,18 +199,44 @@ class Account:
             cvc_input.send_keys("123")
 
             name_input = self.driver.find_element(By.ID, "Field-nameInput")
-            name_input.send_keys("some_name")
+            name_input.send_keys(User.__name__)
 
             address_input = self.driver.find_element(By.CLASS_NAME, "Field-addressLine1Input")
             address_input.send_keys("some_address")
 
+            postal_code_input = self.driver.find_element(By.ID, "Field-postalCodeInput")
+            postal_code_input.send_keys("1832")
+
+            city_input = self.driver.find_element(By.ID, "Field-localityInput")
+            city_input.send_keys("lomas")
+
+            province_dropdown = self.driver.find_element(By.ID, "Field-administrativeAreaInput")
+            province_dropdown.click()
+            provinces_select = self.driver.find_elements(By.TAG_NAME, "option")
+            provinces_select[0].click()
+
+
             submit_btn = self.driver.find_element(By.ID, "btn-submit")
             submit_btn.click()
-            self.driver.implicitly_wait(5)
+            WebDriverWait(self.driver,10).until(
+                EC.visibility_of_element_located(self.BTN_NEXT_ADD_PAY)
+            )
 
-            next_btn = self.driver.find_element(By.ID, "btn-create-listing-creditCardInformation")
+            next_btn = self.driver.find_element(*self.BTN_NEXT_ADD_PAY)
             next_btn.click()
-            self.driver.implicitly_wait(5)
+            WebDriverWait(self.driver,10).until(
+                EC.visibility_of_element_located(self.BTN_NEXT_CREATE_LIST)
+            )
+
+            next_btn = self.driver.find_element(*self.BTN_NEXT_CREATE_LIST)
+            next_btn.click()
+
+            WebDriverWait(self.driver,10).until(
+                EC.visibility_of_element_located(self.BTN_POST)
+            )
+            post_btn = self.driver.find_element(*self.BTN_POST)
+            post_btn.click()
+
         else:
             raise NoPerformersWithEventsError("No performers with events found.")
 
